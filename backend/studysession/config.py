8039554b02cf8dd95b39e1d40dfc3b62617df870
Brian Nielsen <bnielsen1@ufl.edu>
2025-01-25 17:12:51 -0500

    renamed app folder to proper name in backend

--- backend/studysession/config.py
+++
@@ -1,5 +0,0 @@
-
-username = 'andrewperezledo'
-password = 'XowVJcXO1eZzWgXd'
-db = 'StudySeshDB'
-host = 'mongodb+srv://andrewperezledo:' + password + '@studysesh.47wje.mongodb.net/?retryWrites=true&w=majority&appName=StudySesh'
