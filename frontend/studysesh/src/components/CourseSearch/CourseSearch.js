d97c48f0bd9c47f1fdc673e8974c50f2bce9397e
Daniel Romero <romero.daniel@ufl.edu>
2025-01-26 00:35:37 -0500

    Add CourseSearch component and integrate navigation from MainPage

---
+++ frontend/studysesh/src/components/CourseSearch/CourseSearch.js
@@ -0,0 +1,91 @@
+import React,  { useEffect, useState }from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth0 } from "@auth0/auth0-react";
+import config from '../../config/config';
+import styles from './CourseSearch.module.css';
+import axios from 'axios';
+import LoginButton from "../../login";
+import Profile from '../../profile';
+import LogoutButton from '../../logout';
+import SendEmailButton from '../../sendDataButton';
+
+function CourseSearch() {
+  const [id, setId] = useState(-1);
+  const [course, setCourse] = useState(null);
+  const [library, setLibrary] = useState(null);
+  const { isAuthenticated } = useAuth0();
+  const navigate = useNavigate();
+
+  const navigateTo = (path) => {
+    navigate(path);
+  };
+
+  useEffect(() => {
+    axios.get(`http://127.0.0.1:8000/api/sessions/?format=json`)
+      .then(response => {
+        setId(response.data[0].User);
+        setCourse(response.data[0].Course);
+        setLibrary(response.data[0].Library);
+        console.log('Fetched Session:', {id}, {course}, {library});  
+      })
+      .catch(error => {
+        console.error('Error fetching session:', error);
+      });
+  }, [])
+
+  return (
+  
+    <div className={styles.App}>
+      
+      <div className={styles.header}>
+        <div className={styles.logoCircle}>
+          <img src="/images/mdi_library-outline.svg" alt="logo" className={styles.logo}/>
+        </div>
+        <div className={styles.searchBarContainer}>
+          <div className={styles.title}>
+            StudySesh
+          </div>
+          <div className={styles.searchBar}>
+            <input type="text" placeholder="Search..." className={styles.searchInput}/>
+            <button className={styles.searchButton}>
+              <img src="/images/arcticons_searchbar-ex.png" alt="searchIcon" className={styles.searchIcon}/>
+            </button>
+          </div>
+        </div>
+        <div className={styles.buttonContainer}>
+          <button onClick={() => navigateTo('/')} className={styles.logoutButton}>Return</button>
+        </div>
+      </div>
+      <div className={styles.threecolumns}>
+        <div className={styles.leftColumn}>
+          <div className={styles.menuImageContainer}>
+            <img src="/images/TempImage.jpg" alt="leftImage" className={styles.menuImage}/>
+          </div>
+          <div className={styles.courseButtonContainer}>
+            <button className={styles.courseButton}>Search by Course</button>
+          </div>          
+        </div>
+        <div className={styles.middleColumn}>
+        <div className={styles.menuImageContainer}>
+            <img src="/images/TempImage.jpg" alt="middleImage" className={styles.menuImage}/>
+          </div>
+          <div className={styles.sessionButtonContainer}>
+            <button className={styles.sessionButton}>
+              <img src="/images/ic_round-plus.svg" alt="plusSymbol" className={styles.plusSymbol}/>
+            </button>
+          </div>
+        </div>
+        <div className={styles.rightColumn}>
+          <div className={styles.menuImageContainer}>
+            <img src="/images/TempImage.jpg" alt="rightImage" className={styles.menuImage}/>
+          </div>
+          <div className={styles.libraryButtonContainer}>
+            <button className={styles.libraryButton}>Search by Library</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default CourseSearch;
