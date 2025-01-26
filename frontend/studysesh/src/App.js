bb980eeb18c5022544ea7458f3649c3df64b83cc
andrewperezledo <andrewperezledo@gmail.com>
2025-01-26 03:16:15 -0500

    Fiexed Create Session Login

--- frontend/studysesh/src/App.js
+++ frontend/studysesh/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import MainPage from './components/MainPage/MainPage';
 import CourseSearch from './components/CourseSearch/CourseSearch';
+import CreateSession from './components/CreateSession/CreateSession';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage/>}/>
           <Route path="/course-search" element={<CourseSearch/>}/>
+          <Route path="/create" element={<CreateSession/>}/>
         </Routes>
       </div>
     </Router>
--- frontend/studysesh/src/components/MainPage/MainPage.js
+++ frontend/studysesh/src/components/MainPage/MainPage.js
@@ -8,7 +8,7 @@ import LoginButton from "../../login";
 import Profile from '../../profile';
 import LogoutButton from '../../logout';
 import SendEmailButton from '../../sendDataButton';
-import CourseCard from '../../courseCard';
+// import CourseCard from '../../courseCard';
 
 
 function MainPage() {
@@ -21,6 +21,7 @@ function MainPage() {
   const navigateTo = (path) => {
     navigate(path);
   };
+
   useEffect(() => {
     axios.get(`http://127.0.0.1:8000/api/sessions/?format=json`)
       .then(response => {
@@ -82,7 +83,7 @@ function MainPage() {
         {isAuthenticated ? (
             <LogoutButton className={styles.logoutButton} />
           ) : (
-            <LoginButton className={styles.loginButton} />
+            <LoginButton className={styles.loginButton} textOutput="Log In"/>
           )}
         </div>
       </div>
@@ -100,9 +101,13 @@ function MainPage() {
             <img src="/images/TempImage.jpg" alt="middleImage" className={styles.menuImage}/>
           </div>
           <div className={styles.sessionButtonContainer}>
-            <button className={styles.sessionButton}>
+          {isAuthenticated ? (
+            <button onClick={() => navigateTo('/create')} className={styles.sessionButton}>
               <img src="/images/ic_round-plus.svg" alt="plusSymbol" className={styles.plusSymbol}/>
             </button>
+          ) : (
+            <LoginButton className={styles.sessionButton} textOutput={<img src="/images/ic_round-plus.svg" alt="plusSymbol" className={styles.plusSymbol}/>}/>
+          )}
           </div>
         </div>
         <div className={styles.rightColumn}>
