bdbfb16834432b54b7f7d097ca008a9e9e73f7df
dev-par <146295367+dev-par@users.noreply.github.com>
2025-01-25 21:23:22 -0500

    Integrated auth0 into frontend and backend

--- frontend/studysesh/src/App.js
+++ frontend/studysesh/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import LoginButton from "./login";
+import Profile from './profile';
+import LogoutButton from './logout';
 import './App.css';
 
 function App() {
@@ -8,11 +10,14 @@ function App() {
       <div className="title">
         StudySesh
       </div>
+      <LoginButton />
+      <LogoutButton />
       <div className="three-columns">
         <div className="column">Column 1</div>
         <div className="column">Column 2</div>
         <div className="column">Column 3</div>
       </div>
+      <Profile />
     </div>
   );
 }
