389870c3e20b358500df6bfedea21abb5de4ad8b
Daniel Romero <romero.daniel@ufl.edu>
2025-01-25 22:01:50 -0500

    Add color variables, restructure MainPage layout, and integrate new styles

--- frontend/studysesh/src/components/MainPage/MainPage.js
+++ frontend/studysesh/src/components/MainPage/MainPage.js
@@ -3,13 +3,16 @@ import { useNavigate } from 'react-router-dom';
 import config from '../../config/config';
 import styles from './MainPage.module.css';
 import axios from 'axios';
+import LoginButton from "../../login";
+import Profile from '../../profile';
+import LogoutButton from '../../logout';
 
 function MainPage() {
   const [id, setId] = useState(-1);
   const [building, setBuilding] = useState(null);
 
   useEffect(() => {
-    axios.get(`http://127.0.0.1:8000/api/sessions/?format=json`)
+    axios.get(`http://127.0.0.1:8000/api/sessions`)
       .then(response => {
         setId(response.data[0].id);
         setBuilding(response.data[0].building);
@@ -21,19 +24,46 @@ function MainPage() {
   }, [])
 
   return (
+  
     <div className={styles.App}>
-      <div className={styles.title}>
+      <div className={styles.header}>
+        <div className={styles.logoCircle}>
+          <img src="/images/mdi_library-outline.svg" alt="logo" className={styles.logo}/>
+        </div>
+        <div className={styles.title}>
         StudySesh
+        </div>
+        <div className={styles.buttonContainer}>
+          <button className={styles.loginButton}>
+            Log In
+          </button>
+        </div>
       </div>
       <div className={styles.threecolumns}>
-        <div className={styles.column}>
-          Column 1
-          <p>{id}</p>
-          <p>{building}</p>
+        <div className={styles.leftColumn}>
+          <div className={styles.menuImageContainer}>
+            <img src="/images/TempImage.jpg" alt="leftImage" className={styles.menuImage}/>
+          </div>
+          <div className={styles.courseButtonContainer}>
+            <button className={styles.courseButton}>Search by Course</button>
+          </div>          
         </div>
-        <div className={styles.column}>Column 2</div>
-        <div className={styles.column}>Column 3</div>
+        <div className={styles.middleColumn}>Column 2</div>
+        <div className={styles.rightColumn}>
+          Column 3
+        <p>{id}</p>
+        <p>{building}</p>
+        </div>
       </div>
+
+      <LoginButton />
+      <LogoutButton />
+      <div className="three-columns">
+        <div className="column">Column 1</div>
+        <div className="column">Column 2</div>
+        <div className="column">Column 3</div>
+      </div>
+      <Profile />
     </div>
   )
 };
