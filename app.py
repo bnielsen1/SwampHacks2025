8369e46f416cfe53d54b6072c0b7aa92b8a97303
andrewperezledo <andrewperezledo@gmail.com>
2025-01-25 20:36:09 -0500

    UF data scrapping in app.py, example json

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
-print("Hello World!")
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import json
+
+uri = "mongodb+srv://andrewperezledo:XowVJcXO1eZzWgXd@studysesh.47wje.mongodb.net/?retryWrites=true&w=majority&appName=StudySesh"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+db = client["StudySeshDB"]
+uf_data_collection = db["UFdata"]
+
+with open ("example.json", 'r') as file:
+    data = json.load(file)
+
+if not isinstance(data, list):
+    data = [data] 
+
+for document in data:
+    result = uf_data_collection.insert_one(document)
+    print(f"Inserted document with ID: {result.inserted_id}")
+
+client.close()
+
