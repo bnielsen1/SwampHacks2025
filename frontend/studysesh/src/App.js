aa630a316218a7d1a8ecb3470508430fed65af4d
Brian Nielsen <bnielsen1@ufl.edu>
2025-01-26 03:33:35 -0500

    Added library search menu

--- frontend/studysesh/src/App.js
+++ frontend/studysesh/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import MainPage from './components/MainPage/MainPage';
 import CourseSearch from './components/CourseSearch/CourseSearch';
+import LibrarySearch from './components/LibrarySearch/LibrarySearch';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage/>}/>
           <Route path="/course-search" element={<CourseSearch/>}/>
+          <Route path="/library-search" element={<LibrarySearch/>}/>
         </Routes>
       </div>
     </Router>
